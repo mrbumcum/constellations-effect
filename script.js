4f0292ca2d0455b8cc2832368d237adb7a96de92
shucksow <102425362+shucksow@users.noreply.github.com>
2024-10-18 19:32:35 -0400

    feat: draw function to generate particles

--- script.js
+++ script.js
@@ -15,6 +15,12 @@ class Particle {
         this.y = Math.random() * this.effect.height;
         this.radius = 15;
     }
+            
+    draw(context) {
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        context.fill();
+    }
 }
 
 class Effect {
