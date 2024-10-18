88fb56a1aa9474f2113903a972d77b6074c05e85
shucksow <102425362+shucksow@users.noreply.github.com>
2024-10-18 19:31:51 -0400

    feat: Initialize constructors for the classes

--- script.js
+++ script.js
@@ -4,11 +4,26 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-class Particle {
+ctx.fillStyle = 'white';
 
+
+
+class Particle {
+    constructor(effect) {
+        this.effect = effect;
+        this.x = Math.random() * this.effect.width;
+        this.y = Math.random() * this.effect.height;
+        this.radius = 15;
+    }
 }
 
 class Effect {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.height = this.canvas.height;
+        this.width = this.canvas.width;
+        this.particlesArray = [];
+    }
 
 }
 
